04eeb919e43d63d42085b4bfccf16cdf26ffcc30
sajal <sajalsahu8@gmail.com>
2023-11-16 14:00:17 +0530

    integrated home with main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bygrocerriesadminpanel/home.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -7,5 +8,5 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(AdminPanel());
 }
