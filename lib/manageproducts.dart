6c384e5d6b0411cf42885c30e329d37f2fc1a3b1
sajal <sajalsahu8@gmail.com>
2023-12-03 01:00:16 +0530

    created manageproducts

---
+++ lib/manageproducts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ManageProducts extends StatefulWidget {
+  const ManageProducts({super.key});
+
+  @override
+  State<ManageProducts> createState() => _ManageProductsState();
+}
+
+class _ManageProductsState extends State<ManageProducts> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
